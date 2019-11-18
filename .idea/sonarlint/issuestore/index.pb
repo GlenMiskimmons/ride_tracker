0653dec7fe12bce0390822d480f5e644f4f19c2c
LM\N0269887 <glen.miskimmons@liberty-it.co.uk>
2019-11-18 08:55:09 -0500

    Topic 3 Setting up the JDBC Project
    (2)

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+p
+@src/test/java/com/pluralsight/controller/RestControllerTest.java,4\7\47991b65f227754a20b758e222e4de7ffb0e67f6
+p
+@src/main/java/com/pluralsight/repository/RideRepositoryImpl.java,a\e\ae57a86e5df1ba63c6431309c1a1ecc142adb09e
+l
+<src/main/java/com/pluralsight/repository/RideRepository.java,3\b\3ba78648ce57c3c7fa5d2857e119315720175b7f
+l
+<src/main/java/com/pluralsight/controller/RideController.java,e\8\e808ff67413f94b3a0eaff330df5753c18b2c05a
+]
+-src/main/java/com/pluralsight/JdbcConfig.java,2\a\2ada1b44387006a102aa459a09492fd9a64128a5
---
+++ src/main/java/com/pluralsight/JdbcConfig.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class JdbcConfig {
+
+    @Bean
+    public DriverManagerDataSource driverManagerDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/ride_tracker");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(driverManagerDataSource());
+
+        return jdbcTemplate;
+    }
+
+}
